6ce8d5fef6b0d9a3127ca16edbf6f04de71daed1
Deathicecream <1018920338@qq.com>
2018-09-06 10:59:57 +0800

    added c++ solution

--- 3. Longest Substring Without Repeating Characters/main.cpp
+++ 3. Longest Substring Without Repeating Characters/main.cpp
@@ -16,23 +16,47 @@ using namespace std;
 class Solution{
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> m(256, -1);
+        
+        int start = 0;
+        int end = -1;
         int maxlen = 0;
-        int start = -1;
+        vector<int> mymap(256, -1);
         for(int i = 0; i < s.size(); ++i){
-            if(m[s[i]] > start){
-                start = m[s[i]];
+            if(mymap[s[i]] > -1){
+                //cout << "i = " << i << endl;
+
+                start = (start > mymap[s[i]] + 1) ? (start) : (mymap[s[i]] + 1);
+                mymap[s[i]] = -1;
+
             }
-            m[s[i]] = i;
-            maxlen = max(maxlen, i - start);
+            mymap[s[i]] = i;
+            end++;
+            maxlen = (maxlen > end - start + 1) ? (maxlen) : (end - start + 1);
+            //cout << "i = " << i << " maxlen = " << maxlen << " start = " << start << " end = " << end << endl;
+
         }
+        maxlen = (maxlen > end - start + 1) ? (maxlen) : (end - start + 1);
         return maxlen;
+
+        
+//        vector<int> m(256, -1);
+//        int maxlen = 0;
+//        int start = -1;
+//        for(int i = 0; i < s.size(); ++i){
+//            if(m[s[i]] > start){
+//                start = m[s[i]];
+//            }
+//            m[s[i]] = i;
+//            maxlen = max(maxlen, i - start);
+//        }
+//        return maxlen;
     }
 };
 
 int main(int argc, const char * argv[]) {
     Solution S;
-    string s = "pwwkew";
+    //string s = "pwwkew";
+    string s = "abba";
     int length = S.lengthOfLongestSubstring(s);
     cout << length << endl;
     return 0;
